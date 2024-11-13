71809b0b42f4594e3fe71b1956d3cbb301020f27
Vinay <vinaykumar.koudi@smarterp.com>
2024-11-13 21:17:37 +0530

    calculator ui changes

--- src/components/StringCalculator.tsx
+++ src/components/StringCalculator.tsx
@@ -26,7 +26,7 @@ const StringCalculator: React.FC = () => {
 
   return (
     <div className="flex flex-col items-center min-h-screen bg-gradient-to-br from-orange-300 to-purple-400 p-6">
-      <h2 className="text-3xl font-extrabold text-orange-900 mb-10 mt-12 text-center shadow-md px-4 py-2 bg-gradient-to-r from-yellow-400 to-pink-500 text-transparent bg-clip-text">
+      <h2 className="py-4 px-32 text-sm mb-12 font-semibold text-white bg-gradient-to-r from-blue-500 to-green-500 rounded-xl shadow-lg hover:opacity-90 transition duration-300 transform hover:scale-105 flex space-x-4 mt-4">
         String Calculator Assignment
       </h2>
       <form
